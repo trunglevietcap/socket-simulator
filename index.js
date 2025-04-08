006d5a51134f70d236dd3ba02b92ee84d1706422
Trung.le <trung.le@vcsc.com.vn>
2025-04-08 17:54:48 +0700

    update socket

--- index.js
+++ index.js
@@ -109,8 +109,8 @@ io.on("connection", (socket) => {
   });
 });
 
-server.listen(5000, () => {
-  console.log("Server is running on port 5000");
+server.listen(8080, () => {
+  console.log("Server is running on port 8080");
 });
 
-// http://localhost:5000/
+// http://localhost:8080/
